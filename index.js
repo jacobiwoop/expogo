30a02cd423f99b0e6039ccf21522d121a1673d50
jacobiwoop <jacobiwoop@gmail.com>
2026-04-30 19:15:46 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+import { registerRootComponent } from 'expo';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#000' }}>
+      <Text style={{ color: '#fff', fontSize: 18 }}>ExpoWoop Dev Client</Text>
+    </View>
+  );
+}
+
+registerRootComponent(App);
