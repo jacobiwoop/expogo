fa5aea32fe04b31a3081c739a98989a82c715d22
jacobiwoop <jacobiwoop@gmail.com>
2026-05-02 09:17:27 +0100

    Refactor PlaybackService to use module.exports

--- service.js
+++ service.js
@@ -1,9 +1,9 @@
 import TrackPlayer, { Event } from 'react-native-track-player';
 
-export async function PlaybackService() {
-  TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
-  TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
-  TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext());
-  TrackPlayer.addEventListener(Event.RemotePrevious, () => TrackPlayer.skipToPrevious());
-  TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.destroy());
-}
+module.exports = async function() {
+    TrackPlayer.addEventListener(Event.RemotePlay, () => TrackPlayer.play());
+    TrackPlayer.addEventListener(Event.RemotePause, () => TrackPlayer.pause());
+    TrackPlayer.addEventListener(Event.RemoteStop, () => TrackPlayer.destroy());
+    TrackPlayer.addEventListener(Event.RemoteNext, () => TrackPlayer.skipToNext());
+    TrackPlayer.addEventListener(Event.RemotePrevious, () => TrackPlayer.skipToPrevious());
+};
